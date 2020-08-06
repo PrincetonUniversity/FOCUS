7c9649d9b35b93e5529e4aee1391e8a58a4a55fd
Tony Qian <tqian@eddy.princeton.edu>
2020-08-06 08:21:53 -0400

    fixed DPBIN derivative; still debugging

--- sources/bindip.f90
+++ sources/bindip.f90
@@ -24,7 +24,7 @@ SUBROUTINE bindip(ideriv)
   INTEGER, INTENT(in) :: ideriv
 
   INTEGER             :: astat, ierr, icoil, idof, ND, ivec
-  REAL                :: d1L(1:Ndof), norm(1:Ncoils)
+  REAL                :: d1L(1:Ndof), norm(1:Ncoils), pho
 
   dpbin = zero
   !-------------------------------calculate dpbin-------------------------------------------------- 
@@ -32,18 +32,16 @@ SUBROUTINE bindip(ideriv)
      do icoil = 1, Ncoils
         if ( coil(icoil)%Ic /= 0 ) then !if current is free;
            if (coil(icoil)%itype == 2) then
+              pho = coil(icoil)%pho
               if (coil(icoil)%symmetry == 0) then ! no symmetries
                  !pmsum = pmsum + coil(icoil)%I*coil(icoil)%I
-                 dpbin = dpbin &
-                        + ABS(coil(icoil)%pho) * ( 1 - ABS(coil(icoil)%pho) )
+                 dpbin = dpbin + ABS(pho) * ( 1 - ABS(pho) )
               else if (coil(icoil)%symmetry == 1) then ! periodicity
                  !pmsum = pmsum + coil(icoil)%I*coil(icoil)%I*Nfp
-                 dpbin = dpbin &
-                     + ABS(coil(icoil)%pho) * ( 1 - ABS(coil(icoil)%pho) )*NFP
+                 dpbin = dpbin + abs(pho) * ( 1 - abs(pho) ) * Nfp
               else if (coil(icoil)%symmetry == 2) then ! stellarator symmetry
                  !pmsum = pmsum + coil(icoil)%I*coil(icoil)%I*Nfp*2
-                 dpbin = dpbin &
-                     + ABS(coil(icoil)%pho) * ( 1 - ABS(coil(icoil)%pho) )*NFP*2
+                 dpbin = dpbin + abs(pho) * ( 1 - abs(pho) ) * Nfp * 2
               else
                  FATAL( bindip01, .true., unspoorted symmetry option )
               end if 
@@ -52,6 +50,8 @@ SUBROUTINE bindip(ideriv)
      enddo
      call MPI_ALLREDUCE( MPI_IN_PLACE, dpbin, 1, MPI_DOUBLE_PRECISION, MPI_SUM, MPI_COMM_WORLD, ierr )
      !pmsum = pmsum / total_moment
+     !dpbin = dpbin / real(Ncoils - 16) ! hard coded to exclude 16 TF coils
+     !dpbin = dpbin / Ndof ! assumes no other dof
   endif
   !-------------------------------calculate d dpbin / d pho-------------------------------------------------- 
   if ( ideriv >= 1 ) then
@@ -62,21 +62,20 @@ SUBROUTINE bindip(ideriv)
         if ( coil(icoil)%Ic /= 0 ) then !if current is free;
            idof = idof +1
            if (coil(icoil)%itype == 2) then
+              pho = coil(icoil)%pho
               if (coil(icoil)%symmetry == 0) then ! no symmetries
                !  t1V(idof) = 2*coil(icoil)%I*coil(icoil)%moment*momentq &
-               !       &       *(coil(icoil)%pho)**(momentq-1)
-                 t1D(idof) = 2 * ABS(coil(icoil)%pho) * ( 1 - ABS(coil(icoil)%pho) ) &
-                      &        * ( 1 - 2*ABS(coil(icoil)%pho) )
+               !       &       *(coil(icAoil)%pho)**(momentq-1)
+                 t1D(idof) = 2 * pho * (1 - abs(pho) ) * ( 1 - 2*abs(pho) )
+               !  t1D(idof) = 2 * ABS(pho) * (1 - ABS(pho) ) * ( 1 - 2*ABS(pho) )
               else if (coil(icoil)%symmetry == 1) then ! periodicity
                !  t1V(idof) = 2*coil(icoil)%I*coil(icoil)%moment*momentq &
                !       &       *(coil(icoil)%pho)**(momentq-1)*Nfp
-                 t1D(idof) = 2 * ABS(coil(icoil)%pho) * ( 1 - ABS(coil(icoil)%pho) ) &
-                      &        * ( 1 - 2*ABS(coil(icoil)%pho) ) * NFP 
+                 t1D(idof) = 2 * pho * (1 - abs(pho) ) * ( 1 - 2*abs(pho) )*Nfp
               else if (coil(icoil)%symmetry == 2) then ! stellarator symmetry
                !  t1V(idof) = 2*coil(icoil)%I*coil(icoil)%moment*momentq &
                !       &       *(coil(icoil)%pho)**(momentq-1)*Nfp*2
-                 t1D(idof) = 2 * ABS(coil(icoil)%pho) * ( 1 - ABS(coil(icoil)%pho) ) &
-                      &        * ( 1 - 2*ABS(coil(icoil)%pho) ) * NFP*2
+                 t1D(idof) = 2 * pho * (1 - abs(pho) ) * ( 1 - 2*abs(pho) )*Nfp*2
               else
                  FATAL( bindip02, .true., unspoorted symmetry option ) 
                   ! Q: What does this do?
@@ -95,6 +94,7 @@ SUBROUTINE bindip(ideriv)
      FATAL( bindip , idof-dof_offset .ne. ldof, counting error in packing )
      call MPI_ALLREDUCE( MPI_IN_PLACE, t1D, Ndof, MPI_DOUBLE_PRECISION, MPI_SUM, MPI_COMM_WORLD, ierr )
      !t1V = t1V / total_moment
+     !t1D = t1D / Ndof
      !TMPOUT(t1V)
   endif
 
