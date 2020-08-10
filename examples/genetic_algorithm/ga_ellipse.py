a27cdfa2f8e536c5218d035553818a20c961943b
CaoXiang ZHU <zcxiang@gmail.com>
2020-08-10 03:12:22 -0400

    revise evaluation

--- examples/genetic_algorithm/ga_ellipse.py
+++ examples/genetic_algorithm/ga_ellipse.py
@@ -22,7 +22,7 @@ test = FAMUS(comm=comm, extension='ga_ellipse', verbose=True)
 test.initialize()
 self = test
 
-niter=10; npop=10; CXPB=0.5; MUTPB=0.2;cross_rate=0.5; mutate_rate=0.2
+niter=1000; npop=100; CXPB=0.5; MUTPB=0.2;cross_rate=0.5; mutate_rate=0.2
 import random
 from deap import base
 from deap import creator
@@ -33,7 +33,7 @@ from deap import algorithms
 master = False
 if self.globals.myid == self.globals.master:
     master = True
-IND_SIZE = self.globals.ncoils
+IND_SIZE = self.globals.ncoils_total
 # create types
 creator.create("FitnessMin", base.Fitness, weights=(-1.0, ))
 creator.create("Individual", list, fitness=creator.FitnessMin)
@@ -50,15 +50,24 @@ def evaluate(individual):
     dof = np.ravel(self.comm.allgather(individual))
     return self.func(dof), np.count_nonzero(dof)/len(dof)
 def evaluate2(individual, quit=False):
-    while not quit:
+    if master:
+        dof = np.array(individual)
+    else:
+        dof = np.empty(len(individual), dtype=int)
+    while True:
         self.comm.Barrier()
+        self.comm.Bcast(dof, root=self.globals.master)
+        quit = self.comm.bcast(quit, root=self.globals.master)
         fitness = self.func(dof)
         if master:
-            return fitness
+            return fitness,
+        if quit:
+            return fitness,
+
 toolbox.register("mate", tools.cxUniformPartialyMatched, indpb=cross_rate)
 toolbox.register("mutate", tools.mutUniformInt, low=lower, up=upper, indpb=mutate_rate)
 toolbox.register("select", tools.selTournament, tournsize=10)
-toolbox.register("evaluate", evaluate)
+toolbox.register("evaluate", evaluate2)
 # Statics
 stats = tools.Statistics(key=lambda ind: ind.fitness.values)
 stats.register("avg", np.mean)
@@ -67,9 +76,14 @@ stats.register("min", np.min)
 stats.register("max", np.max)       
 # initialization, mutation, crossover
 pop = toolbox.population(n=npop)
-pop, logbook = algorithms.eaSimple(pop, toolbox, cxpb=CXPB, mutpb=MUTPB, ngen=niter, stats=stats, verbose=master)
-fits = [abs(np.sum(np.array(ind.fitness.values)*np.array(ind.fitness.weights))) for ind in pop]
-best = np.argmin(fits)
-dof = np.ravel(self.comm.allgather(best))
-self.unpacking(dof)
+evaluate2(pop[0])
+if master:
+    pop, logbook = algorithms.eaSimple(pop, toolbox, cxpb=CXPB, mutpb=MUTPB, ngen=niter, stats=stats, verbose=master)
+    fits = [abs(np.sum(np.array(ind.fitness.values)*np.array(ind.fitness.weights))) for ind in pop]
+    best = np.array(pop[np.argmin(fits)])
+    print(evaluate2(best, quit=True))
+else:
+    best = np.empty(len(pop[0]), dtype=int)
+self.comm.Bcast(best, root=self.globals.master)
+self.unpacking(best)
 self.save()
