40485da97977b62d7e16001f10e6a7fcd2726171
Caoxiang Zhu <zcxiang@gmail.com>
2021-02-20 14:46:09 +0800

    update python wrapper

--- python/focuspy.py
+++ python/focuspy.py
@@ -5,7 +5,8 @@ import focus
 import numpy as np
 import time
 
-class FOCUSpy(object):
+
+class FOCUS(object):
     def __init__(self, comm=MPI.COMM_WORLD, extension=None, verbose=True):
         """Initialize FOCUSpy class
         Args:
@@ -22,14 +23,14 @@ class FOCUSpy(object):
         focus.globals.ncpu = comm.Get_size()
         # Read input namelist
         if extension is not None:
-            inputFile = extension+'.input'
+            inputFile = extension + ".input"
             assert os.path.exists(inputFile), "File not existed, please check again!"
             focus.globals.ext = extension
             focus.globals.inputfile = inputFile
             focus.read_namelist(inputFile)
         else:
             # assign a temp name and will not read the input file
-            focus.globals.ext = 'focus_tmp'
+            focus.globals.ext = "focus_tmp"
         # check verbose status
         self.verbose = verbose
         self.dumb = False
@@ -38,12 +39,11 @@ class FOCUSpy(object):
         if not self.verbose:
             focus.mute(1)
             self.dumb = True
+        self.fortranlib = focus
         return
 
     def prepare(self, **kwargs):
-        """prepare runs;
-
-        """
+        """prepare runs;"""
         focus.check_input()
         focus.surface()
         focus.rdcoils()
@@ -59,53 +59,52 @@ class FOCUSpy(object):
         Args:
             verbose (bool): if False, suppress the screen output and redirect to a file "tmp.focus_output". Default: True
         Returns:
-            None        
+            None
         """
         # re-check verbose status
         self.verbose = verbose
         if self.verbose:
             # cancel redirect
             if self.dumb:
-                focus.mute(0) 
+                focus.mute(0)
         else:
             # redirect
             if not self.dumb:
                 focus.mute(1)
-                self.dumb = True 
+                self.dumb = True
         # standard run
         focus.focus()
         return
 
     def save(self, **kwargs):
-        famus.saving()
+        focus.saving()
         return
 
     def func(self, x):
-        """FOCUS basic optimization functions
-        """
+        """FOCUS basic optimization functions"""
         n = len(x)
         return focus.myvalue(x, n)
-    
+
     def grad(self, x):
-        """Gradient
-        """
+        """Gradient"""
         n = len(x)
         g = np.zeros(n)
         focus.mygrad(g, x, n)
         return g
-    
+
     def callback(self, x):
         focus.unpacking(x)
         focus.costfun(0)
         focus.output(time.time() - self.time)
         return focus.globals.chi
 
-if __name__== "__main__":
+
+if __name__ == "__main__":
     ext = sys.argv[1]
-    if '.input' in ext:
+    if ".input" in ext:
         ind = ext.index
         ext = ext[:ind]
-    print('Begin to run FAMUS from python with input file at ', ext+'.input')
+    print("Begin to run FAMUS from python with input file at ", ext + ".input")
     comm = MPI.COMM_WORLD
-    test = FOCUSpy(comm=comm, extension=ext, verbose=True)
+    test = FOCUS(comm=comm, extension=ext, verbose=True)
     test.run(verbose=True)
