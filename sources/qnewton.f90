bbfa400fafd3aaefdd613d14ea9a4f0b6219f085
Caoxiang Zhu <zcxiang@gmail.com>
2021-02-22 00:44:13 +0800

    small format fix cont

--- sources/qnewton.f90
+++ sources/qnewton.f90
@@ -222,8 +222,8 @@ SUBROUTINE qnewton
   !TMPOUT( lowbound )
   !TMPOUT( upbound )
   !TMPOUT( nbounds )  
-  if (myid == 0) write(ounit, '("output  : "A6" : "6(A12," ; "))') "iout", "mark", "chi", "dE_norm", &
-       "Bnormal", "PM eff. vol.", "Dip. Binary" 
+  if (myid == 0) write(ounit, '("output  : "A6" : "8(A12," ; "))') "iout", "mark", "chi", "dE_norm", &
+       "Bnormal", "PM eff. vol.", "Dip. Binary", "Total PM Vol.", "Res. Bn_mn"
 !  TQ-EDIT :: avoid printing FOCUS coil metrics which are not used in FAMUS
 !  if (myid == 0) write(ounit, '("output  : "A6" : "8(A12," ; "))') "iout", "time (s)", "chi", "dE_norm", &
 !       "Bnormal", "Bmn harmonics", "tor. flux", "coil length", "PM eff. vol."   
